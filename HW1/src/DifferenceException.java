0530420c0134e6193a5d0eea24cbae90c7d6b632
Katarina Novikova <novikkatarina@gmail.com>
2022-12-28 00:44:12 +0400

    HW1_1

--- HW1/src/DifferenceException.java
+++ HW1/src/DifferenceException.java
@@ -1,5 +1,5 @@
 public class DifferenceException extends Exception {
-    public DifferenceException(String errorMessage) {
-        super(errorMessage);
+    public DifferenceException(int[] a, int[] b) {
+        super(String.format("длина массивов не равны %d %d", a.length, b.length));
     }
 }
